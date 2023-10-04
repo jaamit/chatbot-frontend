a537958dffbf0227e9d0cb51ab9fdce31167c2bd
Amit <amit.jain@simplifyvms.com>
2023-10-04 07:28:20 -0400

    1. Add front end initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [userMessage, setUserMessage] = useState("");
+
+  const sendMessage = async () => {
+    if (userMessage.trim() === "") return;
+
+    setMessages([...messages, { text: userMessage, sender: "user" }]);
+    setUserMessage("");
+
+    try {
+      const response = await axios.post("http://localhost:8000/get_response/", {
+        user_message: userMessage,
+      });
+
+      setMessages([...messages, { text: response.data.bot_response, sender: "bot" }]);
+    } catch (error) {
+      console.error("Error fetching bot response:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chat-container">
+      <div className="chat-window">
+        {messages.map((message, index) => (
+          <div key={index} className={`message ${message.sender}`}>
+            <p>{message.text}</p>
+          </div>
+        ))}
+      </div>
+      <div className="user-input">
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={userMessage}
+          onChange={(e) => setUserMessage(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
